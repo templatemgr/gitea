5579058f2263404d1cda7db68057678e15ab36f7
casjay <git-admin@casjaysdev.pro>
2024-07-21 12:15:13 -0400

    🗃️ Committing everything that changed 🗃️
    init-scripts/05-docker.sh
    init-scripts/08-gitea.sh
    init-scripts/09-nginx.sh
    init-scripts/zz-act_runner.sh

--- init-scripts/08-gitea.sh
+++ init-scripts/08-gitea.sh
@@ -435,7 +435,7 @@ __run_start_script() {
 #!/usr/bin/env sh
 trap 'retVal=\$?;[ -f "\$SERVICE_PID_FILE" ] && rm -Rf "\$SERVICE_PID_FILE";exit \$retVal' ERR
 #
-set -Eeuo pipefail
+set -Eeo pipefail
 # Setting up $cmd to run as ${SERVICE_USER:-root} with env
 retVal=0
 execPid=""
@@ -454,7 +454,7 @@ EOF
 #!/usr/bin/env sh
 trap 'retVal=\$?;[ -f "\$SERVICE_PID_FILE" ] && rm -Rf "\$SERVICE_PID_FILE";exit \$retVal' ERR
 #
-set -Eeuo pipefail
+set -Eeo pipefail
 # Setting up $cmd to run as ${SERVICE_USER:-root}
 retVal=0
 execPid=""
